23afdcf0229e7b40d90e56cb2ca6b590a8e42f1b
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-10-29 01:13:55 +0200

    Remove redundant templates

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <Tutorial/>
+  <h1>This is the main page.</h1>
 </template>
 
 <script lang="ts">
-
 export default {
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
-    };
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
   },
-};
+}
 </script>
