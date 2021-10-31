9350d51827841872c7aa9d7ddc9f2195e82198b8
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-10-31 18:16:37 +0100

    Add a paragraph about our team at the main page and insert our team's logo

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,19 @@
     <section class="first">
       <NavigationButtons />
     </section>
+    <section class="second">
+      <p class="tresc">
+        Jesteśmy zespołem gamedevowym, który składa się z kilkunastu osób
+        pragnących rozwijać swoje umiejętności w zakresie tworzenia gier
+        komputerowych oraz wspólnie pracować nad kilkoma kreatywnymi projektami.
+      </p>
+      <div class="contSec">
+        <div class="left"></div>
+        <div class="right">
+          <img src="../assets/img/maskotka_FO2.png" alt="maskotka" />
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
