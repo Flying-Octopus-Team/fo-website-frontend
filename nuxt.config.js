c33ebc2a03f27b5c7db59551cfbf3137722ec93e
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-01 16:13:08 +0100

    Remove Bootstrap's CDN from the nuxt configuration file

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,11 +29,6 @@ export default {
         crossorigin: "anonymous",
       },
       {
-        src: "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js",
-        integrity: "sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6",
-        crossorigin: "anonymous",
-      },
-      {
         src: "https://kit.fontawesome.com/421d7ccc81.js",
         crossorigin: "anonymous"
       }
