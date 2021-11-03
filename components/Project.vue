9e902d6b06fb4278bee41aabbf531f0efffa9734
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-03 16:08:34 +0100

    Create the Project component, which will contain a single project

---
+++ components/Project.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="grand"></div>
+    <div class="projekt">
+      <div class="projekt-left">
+        <ProjectsCarousel :projectImages="images" />
+      </div>
+      <div class="projekt-right">
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+        <a :href="link">Zagraj</a>
+        <div class="tagi">
+          <div class="tagElementrny">
+            <div class="tagE">itch.io</div>
+          </div>
+          <div class="tagPomocniczy">
+            <div
+              v-for="(tag, index) in tags"
+              class="tagP"
+              :key="index"
+              :style="{ backgroundColor: tag.color }"
+            >
+              {{ tag.name }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    images: Array,
+    tags: Array,
+    link: String,
+    description: String,
+  },
+}
+</script>
+
+<style>
+</style>
