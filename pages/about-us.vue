3f72db76115e72eea39de1afe72c7b3959ee3a62
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-03 16:48:07 +0100

    Create the about-us page

---
+++ pages/about-us.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <DetailedAboutUs />
+  </main>
+</template>
+
+<script lang="ts">
+import DetailedAboutUs from '../components/DetailedAboutUs.vue'
+
+export default {
+  components: {
+    DetailedAboutUs,
+  },
+  mounted() {
+    const navbar = document.querySelector('#navbar')!
+    navbar.classList.remove('fixed-top')
+  },
+}
+</script>
+
+<style>
+</style>
