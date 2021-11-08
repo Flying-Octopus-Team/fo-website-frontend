e9b2a0bd2cb1d9f54a0649e747554f824fadff72
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-08 01:44:39 +0100

    Fix a link to the join us page from the main page

--- components/JoinUs.vue
+++ components/JoinUs.vue
@@ -3,6 +3,6 @@
     <img src="../assets/img/octopus_pictorial_transparent.png" alt="octopus" />
     <h2>Dołącz do nas</h2>
     <p class="textSecKon">Stań się częścią zespołu i twórzmy wspólnie!</p>
-    <a href>Czytaj więcej</a>
+    <a href="/join-us">Czytaj więcej</a>
   </div>
 </template>
