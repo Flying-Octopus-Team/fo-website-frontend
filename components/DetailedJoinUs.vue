e86139ea36c2e6110cc26702d4c9f16e6f14c14a
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-09 01:10:27 +0100

    Add missing content to the DetailedJoinUs component

--- components/DetailedJoinUs.vue
+++ components/DetailedJoinUs.vue
@@ -32,8 +32,46 @@
         </div>
       </div>
     </div>
-    <div class="second-cont-dolacz"></div>
-    <div class="three-cont-dolacz"></div>
+    <div class="cont-dolacz-row">
+      <div class="second-cont-dolacz">
+        <div class="line-dolacz">
+          <div class="line"></div>
+          <h3>PRAKTYKANT</h3>
+        </div>
+        <div class="cont-dolacz">
+          <div class="center-dolacz">
+            <p class="content">
+              Będąc w zespole, zyskasz możliwość tworzenia gier komputerowych i
+              brania udziału w innych, ciekawych projektach. Jeśli nie masz
+              doświadczenia bądź dopiero zaczynasz swoją przygodę w gamedevie, z
+              pewnością pomogą Ci szkolenia organizowane przez naszych mentorów.
+              Członkowie mają realny wpływ na decyzje podejmowane w zespole oraz
+              kierunek rozwoju grupy. Podsumowując, jeśli szukałeś grupy do
+              wspólnego projektowania gier - jesteś we właściwym miejscu.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="three-cont-dolacz">
+        <div class="line-dolacz">
+          <div class="line"></div>
+          <h3>PARTNER</h3>
+        </div>
+        <div class="cont-dolacz">
+          <div class="center-dolacz">
+            <p class="content">
+              Będąc w zespole, zyskasz możliwość tworzenia gier komputerowych i
+              brania udziału w innych, ciekawych projektach. Jeśli nie masz
+              doświadczenia bądź dopiero zaczynasz swoją przygodę w gamedevie, z
+              pewnością pomogą Ci szkolenia organizowane przez naszych mentorów.
+              Członkowie mają realny wpływ na decyzje podejmowane w zespole oraz
+              kierunek rozwoju grupy. Podsumowując, jeśli szukałeś grupy do
+              wspólnego projektowania gier - jesteś we właściwym miejscu.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
