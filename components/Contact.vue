7434ed429eeab295e721697a32bd71ea77bfaffc
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-09 01:46:15 +0100

    Create the Contact component with the team's contact information

---
+++ components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="contact" class="contact">
+    <h2>
+      Jeśli chcesz zostać członkiem Flying Octopus, skontaktuj się z nami!
+    </h2>
+    <h3>Wystarczą trzy proste kroki:</h3>
+    <ol>
+      <li>
+        <span>Napisz do nas</span> - prężnie pracujemy na discordzie, dołącz do
+        nas i przywitaj się!
+      </li>
+      <li>Zrób wyznaczone <span>zadanie wstępne</span></li>
+      <li><span>Twórz</span> z nami wspaniałe projekty</li>
+    </ol>
+  </section>
+</template> 
+
+<script lang="ts">
+export default {}
+</script>
