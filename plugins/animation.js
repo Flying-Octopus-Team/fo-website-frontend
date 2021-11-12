51014c2b8d45197b701083ce2447ab11677d8402
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-12 03:16:26 +0100

    Refactor code

--- plugins/animation.js
+++ plugins/animation.js
@@ -1,11 +1,11 @@
 $(function () { // a self calling function
     function onScrollInit(items, trigger) { // a custom made function
         items.each(function () { // for every element in items run function
-            const osElement = $(this), // set osElement to the current 
-                osAnimationClass = osElement.attr('data-animation'), // get value of attribute data-animation type
-                osAnimationDelay = osElement.attr('data-delay'); // get value of attribute data-delay time
+            const osElement = $(this); // set osElement to the current 
+            const osAnimationClass = osElement.attr('data-animation'); // get value of attribute data-animation type
+            const osAnimationDelay = osElement.attr('data-delay'); // get value of attribute data-delay time
 
-            osElement.css({ //change css of element
+            osElement.css({ // change css of element
                 '-webkit-animation-delay': osAnimationDelay, // for safari browsers
                 '-moz-animation-delay': osAnimationDelay, // for mozilla browsers
                 'animation-delay': osAnimationDelay // normal
@@ -13,7 +13,7 @@ $(function () { // a self calling function
 
             const osTrigger = trigger ?? osElement; // if trigger is present, set it to osTrigger. Else set osElement to osTrigger
 
-            osTrigger.waypoint(function () { //scroll upwards and downwards
+            osTrigger.waypoint(function () { // scroll upwards and downwards
                 osElement.addClass('animated').addClass(osAnimationClass); // add animated and the data-animation class to the element.
             }, {
                 triggerOnce: true, // only once this animation should happen
