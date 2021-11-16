68b17e986b3be72f4442bcbfe800b5ea2dc48a14
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-16 21:58:07 +0100

    Add a link to our discord server in the Contact component

--- components/Contact.vue
+++ components/Contact.vue
@@ -6,8 +6,8 @@
     <h3>Wystarczą trzy proste kroki:</h3>
     <ol>
       <li>
-        <span>Napisz do nas</span> - prężnie pracujemy na discordzie, dołącz do
-        nas i przywitaj się!
+        <a href="https://discord.gg/Q2DuSNY">Napisz do nas</a> - prężnie
+        pracujemy na discordzie, dołącz do nas i przywitaj się!
       </li>
       <li>Zrób wyznaczone <span>zadanie wstępne</span></li>
       <li><span>Twórz</span> z nami wspaniałe projekty!</li>
