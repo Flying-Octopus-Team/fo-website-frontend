eec6a9a0a0eda935918a9c23085e95a0545030cd
Adam Szlósarczyk <contact@adamszlosarczyk.com>
2021-11-16 21:48:31 +0100

    Remove the newsletter link from the AppFooter component

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -7,11 +7,6 @@
       <div class="squer"></div>
       <div class="squer"></div>
     </div>
-    <div class="joinNewsletter">
-      <a href>
-        <p class="pink">Dołącz do naszego newslettera.</p>
-      </a>
-    </div>
     <div class="copyAll">
       <p>&copy; 2021 Flying Octopus. All rights reserved.</p>
       <div class="grad"></div>
