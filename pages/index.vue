4cd5d1d0fc2b3370871453a82ae067f4efbc545c
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2022-04-01 12:30:59 +0200

    #162 Fix long navigation and make members on main page even in width

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,57 @@
 <template>
-  <div class='d-flex flex-column fo-main-container'>
-    <div class='fo-main-title mt-5 mx-5'>
-      <div class='text-center fo-title-img'>
-        <b-img-lazy fluid src='~/assets/img/fo-title.svg' alt='Flying Octopus'></b-img-lazy>
+  <div>
+    <div class='d-flex flex-column fo-main-container'>
+      <div class='fo-main-title mt-5 mx-5'>
+        <div class='text-center fo-title-img'>
+          <b-img-lazy fluid src='~/assets/img/fo-title.svg' alt='Flying Octopus'></b-img-lazy>
+        </div>
+        <div class='d-flex flex-row flex-wrap justify-content-center align-content-center my-2'>
+          <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
+            <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
+          </a>
+          <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
+            <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
+          </a>
+          <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
+            <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
+          </a>
+          <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
+            <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
+          </a>
+          <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
+            <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
+          </a>
+          <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
+            <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
+          </a>
+        </div>
       </div>
-      <div class='d-flex flex-row flex-wrap justify-content-center align-content-center my-2'>
-        <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
-          <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
-        </a>
-        <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
-          <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
-        </a>
-        <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
-          <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
-        </a>
-        <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
-          <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
-        </a>
-        <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
-          <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
-        </a>
-        <a href='' class='m-lg-5 mx-2 my-5 fo-main-nav-btn'>
-          <img class='img-fluid' src='~/assets/img/fo-icon.svg' alt='icon'>
-        </a>
-      </div>
-    </div>
-    <div class='d-flex flex-column fo-main-page-content pt-3'>
-      <h4 class='fo-main-text text-center font-weight-light text-wrap'>  <!--TODO: adjust margins to screen size -->
-        Jesteśmy zespołem gamedevowym, który składa się z kilkunastu osób
-        pragnących rozwijać swoje umiejętności w zakresie tworzenia gier
-        komputerowych oraz wspólnie pracować nad kilkoma kreatywnymi projektami.
-      </h4>
-      <div class='d-flex flex-row align-items-center fo-main-blog'>
-        <BlogPosts class='flex-grow-1 pr-lg-4' :posts='recentBlogPosts' />
-        <b-img-lazy class='img-fluid d-none d-lg-block' src='~/assets/img/smolocti.png' alt='Flying Octopus Mascot' />
-      </div>
-      <div class='fo-main-projects'>
-        <h1 class='text-center fo-main-text m-5'>Nasze projekty</h1>
-        <div class='fo-line-separator my-4'></div>
-        <ProjectsCarousel :project-images='projectImages' />
-        <div class='fo-line-separator my-4'></div>
-      </div>
-      <div id='members' class='fo-main-members'>
-        <h1 class='text-center fo-main-text m-4'>Nasz zespół</h1>
-        <OurTeam :members='activeTeamMembers' />
-      </div>
-      <div class='d-flex flex-column fo-main-join my-4 pt-4'>
-        <b-img class='mx-auto' src='~/assets/img/fo-icon.svg' alt='Flying Octopus Logo' />
-        <h3 class='text-center mt-4'>Dołącz do nas</h3>
-        <h5 class='text-center fo-text-join-bottom mb-4'>Stań się częścią zespołu i twórzmy wspólnie!</h5>
-        <b-link to='/join' class='fo-gradient-button text-center my-4 py-2 px-4'>Czytaj więcej</b-link>
+      <div class='d-flex flex-column fo-main-page-content pt-3'>
+        <h4 class='fo-main-text text-center font-weight-light text-wrap'>
+          Jesteśmy zespołem gamedevowym, który składa się z kilkunastu osób
+          pragnących rozwijać swoje umiejętności w zakresie tworzenia gier
+          komputerowych oraz wspólnie pracować nad kilkoma kreatywnymi projektami.
+        </h4>
+        <div class='d-flex flex-row align-items-center fo-main-blog'>
+          <BlogPosts class='flex-grow-1 pr-lg-4' :posts='recentBlogPosts' />
+          <b-img-lazy class='img-fluid d-none d-lg-block' src='~/assets/img/smolocti.png' alt='Flying Octopus Mascot' />
+        </div>
+        <div class='fo-main-projects'>
+          <h1 class='text-center fo-main-text m-5'>Nasze projekty</h1>
+          <div class='fo-line-separator my-4'></div>
+          <ProjectsCarousel :project-images='projectImages' />
+          <div class='fo-line-separator my-4'></div>
+        </div>
+        <div id='members' class='fo-main-members'>
+          <h1 class='text-center fo-main-text m-4'>Nasz zespół</h1>
+          <OurTeam :members='activeTeamMembers' />
+        </div>
+        <div class='d-flex flex-column fo-main-join my-4 pt-4'>
+          <b-img class='mx-auto' src='~/assets/img/fo-icon.svg' alt='Flying Octopus Logo' />
+          <h3 class='text-center mt-4'>Dołącz do nas</h3>
+          <h5 class='text-center fo-text-join-bottom mb-4'>Stań się częścią zespołu i twórzmy wspólnie!</h5>
+          <b-link to='/join' class='fo-gradient-button text-center my-4 py-2 px-4'>Czytaj więcej</b-link>
+        </div>
       </div>
     </div>
   </div>
