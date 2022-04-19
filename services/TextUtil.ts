73be35606ac1e8c64f2395138640108944a36be2
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2022-04-19 23:48:16 +0200

    Use unicode nonbreaking space

--- services/TextUtil.ts
+++ services/TextUtil.ts
@@ -4,7 +4,7 @@ class TextUtil {
     const lettersToReplace = ['a', 'i', 'o', 'u', 'w', 'z', 'A', 'I', 'O', 'U', 'W', 'Z']
     lettersToReplace.forEach(letter => {
       let textSplit = text.split(' ' + letter + ' ')
-      text = textSplit.join(' ' + letter + '&nbsp;')
+      text = textSplit.join(' ' + letter + '\u00A0')
     })
     return text
   }
