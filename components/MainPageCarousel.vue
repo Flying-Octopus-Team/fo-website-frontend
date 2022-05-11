c98658fb67c7e7b8601e92a77008d53b06a5cfc2
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2022-05-11 12:16:28 +0200

    Switch main page carousel to use new collection

--- components/MainPageCarousel.vue
+++ components/MainPageCarousel.vue
@@ -6,7 +6,11 @@
       controls
       indicators
     >
-      <b-link v-for='(image, index) in projectImages' :key='index' :to='{path: "/projects", hash: "#" + image.link}'>
+      <b-link
+        v-for='(image, index) in carouselImages' :key='index'
+        :href='image.external ? image.link : undefined'
+        :to='image.external ? undefined : {path: "/projects", hash: "#" + image.project}'
+      >
         <b-carousel-slide :key='index' :img-src='image.image'></b-carousel-slide>
       </b-link>
     </b-carousel>
@@ -15,12 +19,12 @@
 
 <script lang='ts'>
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
-import ProjectCarouselImage from '~/types/ProjectCarouselImage'
+import MainCarousel from '~/types/MainCarousel'
 
 @Component
-export default class ProjectsCarousel extends Vue {
+export default class MainPageCarousel extends Vue {
 
   @Prop({ default: () => [] })
-  projectImages!: ProjectCarouselImage[]
+  carouselImages!: MainCarousel[]
 }
 </script>
